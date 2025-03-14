1cbbedec636c06f1a639fd893e03a5a15830af6d
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-03-14 09:45:55 +0100

    URL validation

--- src/validation/profile.ts
+++ src/validation/profile.ts
@@ -3,15 +3,27 @@ import { z } from "zod";
 
 export const linksFormSchema = z.object({
   links: z.array(
-    z.object({
-      platform: z
-        .string()
-        .refine((value) =>
-          platforms.map((platform) => platform.id).includes(value),
-        ),
-      // TODO: Add URL validation
-      url: z.string().min(1, "Can't be empty"),
-    }),
+    z
+      .object({
+        platform: z
+          .string()
+          .refine((value) =>
+            platforms.map((platform) => platform.id).includes(value),
+          ),
+        url: z.string().url("Please check the URL").min(1, "Can't be empty"),
+      })
+      .superRefine((value, ctx) => {
+        const selectedPlatform = platforms.find(
+          (platform) => platform.id === value.platform,
+        );
+        if (!selectedPlatform || !value.url.startsWith(selectedPlatform.url)) {
+          ctx.addIssue({
+            message: "Please check the URL",
+            path: ["url"],
+            code: "custom",
+          });
+        }
+      }),
   ),
 });
 
