19c8fc9d17ac9000cf4ec987acae29c158d36ad9
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-03-21 15:12:23 +0100

    Minor adjustments

--- src/app/(public-view)/[profileId]/page.tsx
+++ src/app/(public-view)/[profileId]/page.tsx
@@ -22,13 +22,11 @@ export default async function UserLinks({ params }: { params: Promise<any> }) {
 
   const { data: profileData } = await getCachedProfile(profileId);
 
-  if (
-    !profileData ||
-    !profileData.firstName ||
-    !profileData.lastName ||
-    !profileData.links
-  )
-    notFound();
+  if (!profileData) notFound();
+
+  const firstName = profileData?.firstName || "John";
+  const lastName = profileData?.lastName || "Apple";
+  const links = profileData?.links || [];
 
   return (
     <Card className="md:bg-background mx-auto max-w-[14.8125rem] gap-0 bg-transparent text-center md:max-w-lg md:min-w-[21.8125rem] md:px-14 md:py-12 md:shadow-[0_0_32px_0] md:shadow-black/10">
@@ -36,24 +34,26 @@ export default async function UserLinks({ params }: { params: Promise<any> }) {
         <div className="flex justify-center">
           <Avatar className="border-primary size-[6.75rem] border-4">
             <AvatarImage
-              src={profileData.avatar}
-              alt={`${profileData.firstName} ${profileData.lastName} profile image`}
+              src={profileData?.avatar}
+              alt={`${firstName} ${lastName} profile image`}
             />
             <AvatarFallback>
-              {`${profileData.firstName.at(0)?.toUpperCase()} ${profileData.lastName.at(0)?.toUpperCase()}`}
+              {`${firstName.at(0)?.toUpperCase()} ${lastName.at(0)?.toUpperCase()}`}
             </AvatarFallback>
           </Avatar>
         </div>
         <CardTitle className="mt-6 text-[2rem] leading-relaxed break-all">
-          {profileData.firstName} {profileData.lastName}
+          {firstName} {lastName}
         </CardTitle>
-        <CardDescription className="mt-2 text-base">
-          {profileData.email}
-        </CardDescription>
+        {!!profileData?.email && (
+          <CardDescription className="mt-2 text-base">
+            {profileData.email}
+          </CardDescription>
+        )}
       </CardHeader>
       <CardContent className="mt-14 px-0">
         <ul className="space-y-5">
-          {profileData.links.map((link) => (
+          {links.map((link) => (
             <PlatformLinkItem
               key={link.platform}
               data={link}
